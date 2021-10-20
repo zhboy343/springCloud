2ead98cf3eb699e6ea9647d510c0560c46fc4c48
zhouhang <1024223787@qq.com>
2021-10-20 21:11:58 +0800

    cloud-provider-payment8001 业务代码-新增、根据id查询

---
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,36 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+public class PaymentController {
+    @Autowired
+    PaymentService paymentService;
+
+    @PostMapping(value = "/payment/create")
+    public CommonResult create(@RequestBody Payment payment){
+        int result = paymentService.create(payment);
+        log.info("*****插入结果："+result);
+        if (result>0){  //成功
+            return new CommonResult(200,"插入数据库成功",result);
+        }else {
+            return new CommonResult(444,"插入数据库失败",null);
+        }
+    }
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id){
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("*****查询结果："+payment);
+        if (payment!=null){  //说明有数据，能查询成功
+            return new CommonResult(200,"查询成功",payment);
+        }else {
+            return new CommonResult(444,"没有对应记录，查询ID："+id,null);
+        }
+    }
+}
