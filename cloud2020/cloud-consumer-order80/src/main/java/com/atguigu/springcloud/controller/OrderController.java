8a8e68eafcaf7227a8ae2cd912bf3661a930e0b2
zhouhang <1024223787@qq.com>
2021-10-25 21:31:32 +0800

    cloud-consumer-order80 业务代码新增、查询订单

--- cloud2020/cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud2020/cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -1,7 +1,35 @@
 package com.atguigu.springcloud.controller;
 
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class OrderController {
+
+    public static final String PAYMENT_URL = "http://localhost:8001";
+
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    // 创建订单
+    @GetMapping("/consumer/payment/create")
+    public CommonResult<Payment> create(@RequestBody Payment payment) {
+        return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+    }
+
+    // 查询订单
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
 }
