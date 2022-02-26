defe6ba4b49d08781e0000c7999dbbfbfdf1fc71
zhouhang <1024223787@qq.com>
2022-02-26 14:15:33 +0800

    hystrix turbine 聚合监控数据

---
+++ cloud2020/cloud-consumer-hystrix-turbine9003/src/main/java/com/atguigu/springcloud/TurbineMain9003.java
@@ -0,0 +1,27 @@
+package com.atguigu.springcloud;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableTurbine
+public class TurbineMain9003 {
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineMain9003.class,args);
+    }
+
+    // 要启用hystrix-dashboard需要注入
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
